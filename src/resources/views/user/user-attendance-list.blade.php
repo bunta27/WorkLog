6186b9442751c0e6530d76aa9a580b5cbde4f6fd
ユーザー名 <>
2025-12-03 19:59:44 +0900

    bladeの書込み

---
+++ src/resources/views/user/user-attendance-list.blade.php
@@ -0,0 +1,66 @@
+@extends('layout.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/user/user-attendance-list.css') }}">
+@endsection
+
+@section('content')
+<div class="attendance-list__content">
+    <div class="content__header">
+        <h2 class="content__header--item">勤怠一覧</h2>
+    </div>
+    <div class="content__menu">
+        <a class="previous-month" href="?date={{ $previousMonth }}">前月</a>
+        <p class="current-month">{{ $date->format('Y-m') }}</p>
+        @if ($date->lt(\Carbon\Carbon::now()))
+            <a class="next-month" href="?date={{ $nextMonth }}">翌月</a>
+        @else
+            <div class="next-month-placeholder"></div>
+        @endif
+    </div>
+    <table class="table">
+        <tr class="table__row">
+            <th class="table__header">
+                <p class="table__header--item">日付</p>
+            </th>
+            <th class="table__header">
+                <p class="table__header--item">出勤</p>
+            </th>
+            <th class="table__header">
+                <p class="table__header--item">退勤</p>
+            </th>
+            <th class="table__header">
+                <p class="table__header--item">休憩</p>
+            </th>
+            <th class="table__header">
+                <p class="table__header--item">合計</p>
+            </th>
+            <th class="table__header">
+                <p class="table__header--item">詳細</p>
+            </th>
+        </tr>
+        @foreach($formattedAttendanceRecords as $attendanceRecord)
+        <tr class="table__row">
+            <td class="table__description">
+                <p class="table__description--item">{{ $attendanceRecord['date'] }}</p>
+            </td>
+            <td class="table__description">
+                <p class="table__description--item">{{ $attendanceRecord['crock_in'] }}</p>
+            </td>
+            <td class="table__description">
+                <p class="table__description--item">{{ $attendanceRecord['clock_out'] }}</p>
+            </td>
+            <td class="table__description">
+                <p class="table__description--item">{{ $attendanceRecord['total_break_time'] }}</p>
+            </td>
+            <td class="table__description">
+                <p class="table__description--item">{{ $attendanceRecord['total_time'] }}</p>
+            </td>
+            <td class="table__description">
+                <a class="table__item--detail--link" href="{{ url('/attendance/' . $attendanceRecord['id']) }}">詳細</a>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+</div>
+@endsection
---
+++ src/resources/views/user/user-detail.blade.php
@@ -0,0 +1,126 @@
+@extends('layout.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/user/user-detail.css') }}">
+@endsection
+
+@section('content')
+<div class="detail__content">
+    <div class="detail__header">
+        <h2 class="detail__header--item">勤怠詳細</h2>
+    </div>
+    <form class="form" action="{{ url('/attendance/ . $data['id']) }}" method="post">
+        @csrf
+        @if(is_null($data['application']))
+            <div class="form__content">
+                <div class="form__group">
+                    <p class="form__header">名前</p>
+                    <input class="form__input form__input--name" type="text" name="name" value="{{ $user->name }}" readonly>
+                </div>
+                <div class="form__group">
+                    <p class="form__header">日付</p>
+                    <input class="form__input" type="text" value="{{ $data['year'] }}" readonly>
+                    <input class="form__input" type="text" name="new_date" value="{{ $data['date'] }}">
+                </div>
+                <div class="form__group">
+                    <p class="form__header">出勤・退勤</p>
+                    <input class="form__input" type="text" name="new_clock_in" value="{{ $data['clock_in'] }}">
+                    <p>～</p>
+                    <input class="form__input" type="text" name="new_clock_out" value="{{ $data['clock_out'] }}">
+                </div>
+                <div class="error__message">
+                    <div class="error__message--item">
+                        @error('new_clock_in')
+                        {{ $message }}
+                        @enderror
+                        @error('new_clock_out')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+                <div class="form__group form__break-group">
+                    <p class="form__header">休憩</p>
+                    <div class="form__input form__input-wrapper">
+                        @foreach($data['breaks'] as $break)
+                            <div class="form__input-group">
+                                <input class="form__input" type="text" name="new_break_in[]" value="{{ $break['break_in'] }}">
+                                <p>～</p>
+                                <input class="form__input" type="text" name="new_break_out[]" value="{{ $break['break_out'] }}">
+                            </div>
+                        @endforeach
+                        <div class="form__input-group">
+                            <input class="form__input" type="text" name="new_break_in[]" value="">
+                            <p>～</p>
+                            <input class="form__input" type="text" name="new_break_out[]" value="">
+                        </div>
+                    </div>
+                </div>
+                <div class="error__message">
+                    <div class="error__message--item">
+                        @foreach($errors->get('new_break_in.*') as $messages)
+                            @foreach((array) $messages as $message)
+                                <p>{{ $message }}</p>
+                            @endforeach
+                        @endforeach
+                        @foreach($errors->get('new_break_out.*') as $messages)
+                            @foreach((array) $messages as $message)
+                                <p>{{ $message }}</p>
+                            @endforeach
+                        @endforeach
+                    </div>
+                </div>
+                <div class="form__group">
+                    <p class="form__header">備考</p>
+                    <textarea class="form__textarea" type="text" name="comment" value="{{ $data['comment'] }}">
+                </div>
+                <div class="error__message">
+                    @error('comment')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            <div class="form__button">
+                <button class="form__button--submit" type="submit">修正</button>
+            </div>
+
+        @else
+            <div class="form__content">
+                <div class="form__group">
+                    <p class="form__header">名前</p>
+                    <input class="form__input form__input--name readonly" type="text" name="name" value="{{ $user->name }}" readonly>
+                </div>
+                <div class="form__group">
+                    <p class="form__header">日付</p>
+                    <input class="form__input readonly" type="text" value="{{ $data['year'] }}" readonly>
+                    <input class="form__input readonly" type="text" value="{{ $data['date'] }}" readonly>
+                </div>
+                <div class="form__group">
+                    <p class="form__header">出勤・退勤</p>
+                    <input class="form__input readonly" type="text" value="{{ $data['clock_in'] }}" readonly>
+                    <p>～</p>
+                    <input class="form__input readonly" type="text" value="{{ $data['clock_out'] }}" readonly>
+                </div>>
+                <div class="form__group form__break-group">
+                    <p class="form__header">休憩</p>
+                    <div class="form__input-wrapper">
+                        @foreach($data['breaks'] as $break)
+                            <div class="form__input-group">
+                                <input class="form__input readonly" type="text" value="{{ $break['break_in'] }}" readonly>
+                                <p>～</p>
+                                <input class="form__input readonly" type="text" value="{{ $break['break_out'] }}" readonly>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="form__group">
+                    <p class="form__header">備考</p>
+                    <textarea class="form__textarea readonly" type="text" name="comment" value="{{ $data['comment'] }}" readonly>
+                </div>
+                <div class="form__button">
+                    <p class="readonly-message">承認待ちのため修正はできません。</p>
+                </div>
+            </div>
+        @endif
+    </form>
+</div>
+@endsection
