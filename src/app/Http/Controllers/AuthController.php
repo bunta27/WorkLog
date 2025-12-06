38d3d242241c7aca944b318baf9fe2a91240669d
ユーザー名 <>
2025-12-06 18:01:21 +0900

    Fortifyの導入、調整

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -7,9 +7,9 @@ use App\Http\Requests\AdminLoginRequest;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Support\Facades\Auth;
-
+use App\Actions\Fortify\CreateNewUser;
 
 class AuthController extends Controller
 {
-    //
+    
 }
